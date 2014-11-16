b7a08acb65eaea72154f41c818f06843043848bc
William Gray Roncal <willgray@jhu.edu>
2014-11-15 22:02:24 -0500

    Initial Truthing Code Upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 neuronID
 ========
+This is the neuronID toolbox, used to identify cell bodies 
+and major projections in a variety of datatypes.  Readers and converters 
+to solve a variety of common system engineering challenges are also provided.
+
+This is an initial version of the toolbox, under active development.  Currently the 
+only functionality supported is the ability to read in data from an OCP project, annotate
+truth data, and save the result as a series of PNG stacks. 
---
+++ io/put_truthing_data.m
@@ -0,0 +1,62 @@
+function put_truthing_data(niftiLabels, outputDir, ignoreID)
+
+% INPUTS:
+% niftiLabels:  NIFTI File from ITK Snap Truthing (full path)
+% OutputDir:  Output Directory
+% ignoreID:  Optional ID to ignore in final results
+
+% OUTPUTS:
+% PNG Stack (one per input slice)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% COPYRIGHT NOTICE
+% (c) 2014 The Johns Hopkins University / Applied Physics Laboratory
+% All Rights Reserved.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Load Data
+
+% Load nii
+filename = fullfile(data_dir,'lables.nii');
+nii = load_nii(filename);
+
+data = nii.img;
+clear nii;
+data = permute(data,[2,1]);
+data = fliplr(data);
+data = flipud(data);
+
+% fill holes
+data = imfill(data);
+
+% Break into IDs and connected component
+ids = unique(data);
+
+% Relabel all cells
+cell_data = zeros(size(data));
+cnt = 2;
+for ii = 2:length(ids)
+  
+   tdata = data;
+   tdata(tdata ~= ids(ii)) = 0;
+   cc = bwconncomp(tdata);
+   
+   if ii == ignoreID
+       for jj = 1:cc.NumObjects
+           cell_data(cc.PixelIdxList{jj}) = 1;
+       end
+   else       
+       for jj = 1:cc.NumObjects
+           cell_data(cc.PixelIdxList{jj}) = cnt;
+           cnt = cnt + 1;
+       end
+   end
+end
+
+
+%% Save PNG Stack
+
+for i = 1:size(cell_data,3)
+    filename = fullfile(outputDir, 'truthLabels_' num2str(i),'.png');
+    imwrite(cell_data(:,:,i), filename)
+end
